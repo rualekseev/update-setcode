b84db38b92f962e7b2e388460ed237818c2daa1a
Aleksandr Alekseev <rualekseev@gmail>
2024-08-05 09:35:38 +0000

    up balance to 5

--- contracts/deploy/setcode.ts
+++ contracts/deploy/setcode.ts
@@ -11,7 +11,7 @@ export default async () => {
         contract: "SetcodeMultisigWallet",
         publicKey: signer1!.publicKey,
         constructorParams: { owners: [`0x${signer1!.publicKey}`, `0x${signer2!.publicKey}`, `0x${signer3!.publicKey}`,`0x${signer4!.publicKey}`, `0x${signer5!.publicKey}`], reqConfirms: 3 },
-        value: locklift.utils.toNano(2)
+        value: locklift.utils.toNano(5)
     },
     deploymentName: "setcode",// user-defined custom name
     },
